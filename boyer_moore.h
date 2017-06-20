ed392fbf98b51fa76ba5d702b1acb666ec8378a3
Yaz Saito <yasushi.saito@gmail.com>
2017-06-19 21:02:00 -0700

    Linted.

--- boyer_moore.h
+++ boyer_moore.h
@@ -46,38 +46,46 @@ class BoyerMooreSearch {
 
   static std::vector<int> FillSuffixes(const std::string& pattern) {
     const int N = pattern.size();
-    std::vector<int> suff(pattern.size());
-    suff[N - 1] = N;
+    std::vector<int> suffixes(pattern.size());
+    suffixes[N - 1] = N;
     int g = N - 1;
     int f = 0;
     for (int i = N - 2; i >= 0; --i) {
-      if (i > g && suff[i + N - 1 - f] < i - g)
-        suff[i] = suff[i + N - 1 - f];
-      else {
-        if (i < g) g = i;
+      if (i > g && suffixes[i + N - 1 - f] < i - g) {
+        suffixes[i] = suffixes[i + N - 1 - f];
+      } else {
+        if (i < g) {
+          g = i;
+        }
         f = i;
-        while (g >= 0 && pattern[g] == pattern[g + N - 1 - f]) --g;
-        suff[i] = f - g;
+        while (g >= 0 && pattern[g] == pattern[g + N - 1 - f]) {
+          --g;
+        }
+        suffixes[i] = f - g;
       }
     }
-    return suff;
+    return suffixes;
   }
+
   void InitGoodSuffixes(const std::string& pattern) {
-    std::vector<int> suff = FillSuffixes(pattern);
+    std::vector<int> suffixes = FillSuffixes(pattern);
     const int N = pattern.size();
     good_suffixes_.resize(N);
     for (int i = 0; i < N; ++i) {
       good_suffixes_[i] = N;
     }
     int j = 0;
-    for (int i = N - 1; i >= 0; --i)
-      if (suff[i] == i + 1)
-        for (; j < N - 1 - i; ++j)
+    for (int i = N - 1; i >= 0; --i) {
+      if (suffixes[i] == i + 1) {
+        for (; j < N - 1 - i; ++j) {
           if (good_suffixes_[j] == N) {
             good_suffixes_[j] = N - 1 - i;
           }
+        }
+      }
+    }
     for (int i = 0; i <= N - 2; ++i) {
-      good_suffixes_[N - 1 - suff[i]] = N - 1 - i;
+      good_suffixes_[N - 1 - suffixes[i]] = N - 1 - i;
     }
   }
   const std::string pattern_;
